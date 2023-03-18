713c222eb96a30736ecb9919bd2b6e811d334831
Bhushan D. Kadu <bkadu3@gmail.com>
2023-03-18 09:46:43 +0530

    Delete scripts.js

--- JS/scripts.js
+++
@@ -1,6 +0,0 @@
-function doSomething() {
-    if (!document.getElementById("demo").innerHTML)
-        document.getElementById("demo").innerHTML = "Hello JavaScript!";
-    else
-        document.getElementById("demo").innerHTML = ""
-}
